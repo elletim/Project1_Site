0ff4968298d22d8d7572ca6c59bc1d03c3ed23b9
elletim <113080146+elletim@users.noreply.github.com>
2022-10-02 18:50:30 -0700

    updated model, created new views and templates

--- polls/models.py
+++ polls/models.py
@@ -8,10 +8,6 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    choice_url = models.CharField(max_length=500)
     def __str__(self):
         return self.choice_text
-class Url(models.Model):
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
-    url_text = models.CharField(max_length=500)
-    def __str__(self):
-        return self.url_text
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 import pandas as pd 
 import requests as req
 from io import StringIO
+from .models import Choice, Question
+from django.template import loader
+from django.urls import reverse
+
 
 
 def getdata(request):
-    context = {
-        "urls" : ["http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/Los_Angeles.txt", 
-        "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/San_Diego.txt",
-       "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/New_York/New_York.txt",
-       "http://berkeleyearth.lbl.gov/air-quality/maps/cities/India/NCT/New_Delhi.txt"],
-    }
     #page = req.get(url)
     #page_df= pd.read_csv(StringIO(page.text),skiprows=10,sep='\t')
     #page_df.columns= ['Year','Month','Day', 'UTC Hour','PM2.5','PM10_mask','Retrospective']     
@@ -24,5 +22,25 @@ def getdata(request):
     #global data 
     #data = pivot.to_html()
     #return HttpResponse(data)
+    #return render(request, 'polls/getdata.html', context)
+    url_list = Choice.objects.all()
+    context= {
+        "url_list" : url_list,
+    }
     return render(request, 'polls/getdata.html', context)
 
+#page that displays question text for poll
+def detail(request, question_id): 
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+    
+
+def results(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('results', args=(question.id,)))
