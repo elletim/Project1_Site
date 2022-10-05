62d81c022f09625d971db6287b20b48e49a3ead0
elletim <113080146+elletim@users.noreply.github.com>
2022-10-04 21:31:05 -0700

    got city dataframe in results

--- polls/views.py
+++ polls/views.py
@@ -37,13 +37,24 @@ def detail(request, question_id):
 def vote(request, question_id):
     question = Question.objects.get(pk=question_id)
     selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    #selected_choice = selected_choice.choice_url
-    selected_choice.save()
-    context = { 'selected_choice': selected_choice,'question': question, }
-    return render(request, 'polls/vote.html', context)
+    urlname = selected_choice.choice_url
+    page = req.get(urlname)
+    page_df= pd.read_csv(StringIO(page.text),skiprows=10,sep='\t')
+    page_df.columns= ['Year','Month','Day', 'UTC Hour','PM2.5','PM10_mask','Retrospective']     
+    page_df = page_df[["Year","Month","PM2.5"]]
+    page_df = page_df[(page_df["Year"].isin([2018,2019,2020,2021,2022])) & (page_df["Month"].isin([3,4,5]))]
+    page_df = page_df.groupby(["Year","Month"])
+    page_df = page_df["PM2.5"].mean()
+    df = pd.DataFrame(data=page_df)
+    pivot = df.pivot_table(columns="Year",index="Month",values="PM2.5")
+    data = pivot.to_html()
+    #context = { 'data': data, 'selected_choice': selected_choice, }
+    return HttpResponse(data)
+    #return render(request, 'polls/vote.html', context)
     # return HttpResponseRedirect(reverse('results', args=(question.id,)),context)
 
+#not currently using this
 def results(request, question_id):
-    question = Question.objects.get(pk=question_id)
-    #return render(request, 'polls/results.html', {'question': question})
+    url = request.POST['urlname']
+    return render(request, 'polls/results.html', {'url': url})
 
