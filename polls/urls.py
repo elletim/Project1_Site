b639f11f2fc63e6c189d5dc88f2c843318fd8714
elletim <113080146+elletim@users.noreply.github.com>
2022-12-03 17:57:29 -0800

    update database

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getdata, name='getdata'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,31 +4,8 @@ import pandas as pd
 import requests as req
 from io import StringIO
 from .models import Choice, Question
-from django.template import loader
 from django.urls import reverse
 import json
-
-
-
-def getdata(request):
-    #page = req.get(url)
-    #page_df= pd.read_csv(StringIO(page.text),skiprows=10,sep='\t')
-    #page_df.columns= ['Year','Month','Day', 'UTC Hour','PM2.5','PM10_mask','Retrospective']     
-    #page_df = page_df[["Year","Month","PM2.5"]]
-    #page_df = page_df[(page_df["Year"].isin([2018,2019,2020,2021,2022])) & (page_df["Month"].isin([3,4,5]))]
-    #page_df = page_df.groupby(["Year","Month"])
-    #page_df = page_df["PM2.5"].mean()
-    #df = pd.DataFrame(data=page_df)
-    #pivot = df.pivot_table(columns="Year",index="Month",values="PM2.5")
-    #global data 
-    #data = pivot.to_html()
-    #return HttpResponse(data)
-    #return render(request, 'polls/getdata.html', context)
-    url_list = Choice.objects.all()
-    context= {
-        "url_list" : url_list,
-    }
-    return render(request, 'polls/getdata.html', context)
 
 #page that displays question text for poll
 def detail(request, question_id): 
@@ -44,21 +21,15 @@ def vote(request, question_id):
     page_df= pd.read_csv(StringIO(page.text),skiprows=10,sep='\t')
     page_df.columns= ['Year','Month','Day', 'UTC Hour','PM2.5','PM10_mask','Retrospective']     
     page_df = page_df[["Year","Month","PM2.5"]]
-    page_df = page_df[(page_df["Year"].isin([2018,2019,2020,2021,2022])) & (page_df["Month"].isin([3,4,5]))]
-    #x = page_df.groupby(["Year","Month"])
-    #print(x)
+    page_df = page_df[(page_df["Year"].isin([2018,2019,2020,2021,2022])) & (page_df["Month"].isin([3,4,5,6]))]
     page_df = page_df.groupby(["Year","Month"])
     page_df = page_df["PM2.5"].mean()
     df = pd.DataFrame(data=page_df)
     y = df["PM2.5"].to_list()
     x = df.index.values
     x = list(x)
-    print(x)
-    #pivot = df.pivot_table(columns="Year",index="Month",values="PM2.5")
-    #pivot = pivot.to_html()
-    #print(pivot.head())
-    #data_json = pivot.to_json()
-    context= { 'data_json' : y, 'x': x, 'question' : question, 'city': selected_choice, }
-    #return HttpResponse(data_json)
+    x = [str(i) for i in x]
+    x = [i.replace(',','/').replace(' ','') for i in x]
+    context= { 'data_json' : y, 'x': json.dumps(x), 'question' : question, 'city': selected_choice, }
     return render(request, 'polls/vote.html', context)
 
